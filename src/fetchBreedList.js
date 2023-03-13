08721b08701706da7b095466dae70949b8feef75
Zarif Hosen <zarifhosen2015@gmail.com>
2023-03-13 21:49:15 +0600

    bug found. Little console bug, fucntion works fine

---
+++ src/fetchBreedList.js
@@ -0,0 +1,13 @@
+const fetchBreedList = async ({ queryKey }) => {
+    const animal = queryKey[1];
+
+    const apiRes = await fetch(`https://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+
+    if (!apiRes.ok) {
+        throw new Error(`Could not fetch ${animal}`);
+    }
+
+    return apiRes.json();
+}
+
+export default fetchBreedList;
